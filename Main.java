4407151505951167b8395e50c6d2a6a1d82b45ec
Qinkuer <1165742548@qq.com>
2021-08-05 14:58:17 +0800

    8.5

--- Main.java
+++ Main.java
@@ -1,5 +1,40 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Main {
-    
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+
+        if (otherArgs.length != 2) {
+            System.err.println("Usage: wordcount <in> <out>");
+            System.exit(2);
+        }
+        Job job = new Job(conf, "word count");// Job(Configuration conf,String jobName)设置job名称
+        job.setJarByClass(WordCount.class);// 为job设置Mapper类
+
+        job.setMapperClass(TokenizerMapper.class);
+
+        job.setReducerClass(IntSumReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));// 为map-reduce任务设置InputFormat实现类，设置输入路径
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));// 为map-reduce任务设置OutputFormat实现类，设置输出路径
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
