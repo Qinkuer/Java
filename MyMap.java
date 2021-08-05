dd0ced411b01fd8c3bb4543efd12263a1f033153
Qinkuer <1165742548@qq.com>
2021-08-05 15:01:41 +0800

    8.6

--- MyMap.java
+++ MyMap.java
@@ -18,7 +18,7 @@ public static class MyMap extends Mapper<Object, Text, Text, IntWritable> {
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
-        StringTokenizer itr = new StringTokenizer(value.toString());
+        StringTokenizer itr = new StringTokenizer(value.toString()," , ! ? ; : ");
         while (itr.hasMoreTokens()) {
             word.set(itr.nextToken());
             context.write(word, one);
